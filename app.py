eff14dfde822682cb5ec400dea9d2067298eca06
alanimmanuel0901 <alanimmanuel768@gmail.com>
2026-03-01 00:28:37 +0530

    fixed mail config

--- app.py
+++ app.py
@@ -20,14 +20,13 @@ db = SQLAlchemy(app)
 # MAIL CONFIGURATION
 # ===========================
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+app.config['MAIL_PORT'] = int(os.environ.get("MAIL_PORT", 587))
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 
 mail = Mail(app)
-
 # ===========================
 # UPLOAD FOLDER
 # ===========================
