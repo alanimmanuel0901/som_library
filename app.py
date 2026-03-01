c8fd93c4317095c254cba2b30105712bb1bb4dc4
alanimmanuel0901 <alanimmanuel768@gmail.com>
2026-03-01 10:32:48 +0530

    Added requirments

--- app.py
+++ app.py
@@ -340,6 +340,7 @@ with app.app_context():
 # RUN
 # ===========================
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
