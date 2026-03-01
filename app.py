2e8523df5e7607cdf1af3fbd3536d2a07e45d038
alanimmanuel0901 <alanimmanuel768@gmail.com>
2026-03-01 21:34:28 +0530

    Final database config

--- app.py
+++ app.py
@@ -14,8 +14,22 @@ app.secret_key = "supersecretkey"
 # DATABASE CONFIG
 # ===========================
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# DATABASE CONFIG
+import os
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    # fallback for local testing
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
 # ===========================
